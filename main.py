5137dc06c0598b283a94e2963149bb9e7a1add6a
Pichler, Olaf <olaf.pichler@urz.uni-heidelberg.de>
2020-07-02 18:56:07 +0200

    first working version of a grammar

---
+++ main.py
@@ -0,0 +1,21 @@
+
+from lark import Lark
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+with open("fortigate.lark", "r") as f:
+    grammar = f.read()
+
+with open("../FW-UV_db.conf", "r") as f:
+    conf = f.read()
+
+parser = Lark(grammar,
+              parser="lalr",
+              propagate_positions=True,
+              start="root",
+              debug=True)
+
+parsed_conf = parser.parse(conf)
+print(parsed_conf)
+
